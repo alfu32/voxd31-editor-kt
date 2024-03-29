83f77679dd8276ab02ff5b49ee40ac299ae4f233
alferio <ioan-cristian.alferaru@ec.europa.eu>
2024-03-29 06:33:50 +0100

    fix(object): object detection

--- core/src/com/voxd31/editor/Voxd31Editor.kt
+++ core/src/com/voxd31/editor/Voxd31Editor.kt
@@ -170,15 +170,15 @@ class Voxd31Editor : ApplicationAdapter() {
         cameraController.update()
         camera.update()
 
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+
         shadowLight.begin(Vector3.Zero, camera.direction)
         shadowBatch.begin(shadowLight.camera)
             scene.cubes.map{ (k,v) -> v.instance}.forEach { shadowBatch.render(it) }
             shadowBatch.render(ground)
         shadowBatch.end()
         shadowLight.end()
-
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
 
 
         modelBatch.begin(camera)
