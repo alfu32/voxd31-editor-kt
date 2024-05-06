19660cd3e99c7a9da22194d86ed109f88f743058
alfu32 <ioan.alferaru@gmail.com>
2024-05-06 09:03:06 +0200

    feat(unit-testing): WIP

--- desktop/src/com/voxd31/editor/DesktopLauncher.java
+++ desktop/src/com/voxd31/editor/DesktopLauncher.java
@@ -7,12 +7,14 @@ import com.xovd3i.editor.Voxd31Editor;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Voxd31EditorVersion version = new Voxd31EditorVersion();
+
+		System.out.println(String.format("version %s %s",version.getBuildDate(),version.getBuildVersion()));
 		System.out.println(String.format("usage : v0x3d1 <resolution>[1280x1024] <filename>",arg.length));
 		System.out.println(String.format("arguments %d",arg.length));
 		System.out.println(String.join(",",arg));
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("voxd31-editor");
 		config.setResizable(true);
 		config.setDecorated(true);
 
@@ -29,6 +31,13 @@ public class DesktopLauncher {
 		if(arg.length > 1) {
 			filename=arg[1];
 		}
+		config.setTitle(String.format(
+				" V0XD31   verision : %s   file : [%s]",
+				version.getBuildVersion(),
+				filename
+		));
+		config.setWindowIcon("voxd31.icon.png");
+		// width,height,
 		new Lwjgl3Application(new Voxd31Editor(filename), config);
 	}
 }
--- dist.sh
+++ dist.sh
@@ -6,6 +6,25 @@ export SDKMAN_DIR="$HOME/.sdkman"
 tag=$1
 
 echo $tag
+
+BUILD_RUNDATE=$(date '+%Y-%m-%d %H:%M:%S')
+GIT_COMMIT=$(git rev-parse HEAD)
+GIT_BRANCH="$(git branch | egrep "^\* ")"
+GIT_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+export VERSIONFILE=core/src/main/kotlin/com/voxd31/editor/Voxd31EditorVersion.kt
+cat > $VERSIONFILE <<VERSIONCLASS
+package com.voxd31.editor
+
+data class Voxd31EditorVersion(
+  val buildGitCommit:String = "$GIT_COMMIT",
+  val buildDate:String = "$BUILD_RUNDATE",
+  val buildGitBranch:String = "${GIT_BRANCH/\*\ /}",
+  val buildGitTag:String = "$GIT_TAG",
+  val buildVersion:String = "$tag"
+)
+VERSIONCLASS
+
 # Define a list of version strings
 versions="8.0.402-tem 11.0.22-tem 17.0.10-tem 21.0.2-tem"
 rm -rf ./dist/*.jar
@@ -19,6 +38,7 @@ do
     ./gradlew clean dist  "-PjavaCompatVersion=$major_version" -PreleaseNumber=$tag
     mv desktop/build/libs/*.jar dist/
     ## git add -f dist/voxd31-editor-desktop-jvm$major_version-$tag.jar
+    cp ./assets/voxd31.icon.png  dist/
 done
 
 
